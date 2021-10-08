a79c2265cda128c4920c7f38ebd4d7fcae80675d
lesismal <lesismal@gmail.com>
2021-10-08 12:09:44 +0800

    update echo example

--- echo_server/echo_server.go
+++ echo_server/echo_server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -14,8 +13,6 @@ import (
 	"github.com/lesismal/nbio/nbhttp/websocket"
 )
 
-var useStd = flag.Bool("std", false, "whether using std server")
-
 func onHello(c echo.Context) error {
 	return c.String(http.StatusOK, "hello world")
 }
@@ -24,15 +21,16 @@ func onWebsocket(c echo.Context) error {
 	w := c.Response()
 	r := c.Request()
 	upgrader := websocket.NewUpgrader()
+	upgrader.OnMessage(func(c *websocket.Conn, messageType websocket.MessageType, data []byte) {
+		// echo
+		c.WriteMessage(messageType, data)
+	})
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return err
 	}
 	wsConn := conn.(*websocket.Conn)
-	wsConn.OnMessage(func(c *websocket.Conn, messageType websocket.MessageType, data []byte) {
-		// echo
-		c.WriteMessage(messageType, data)
-	})
 	wsConn.OnClose(func(c *websocket.Conn, err error) {
 		log.Println("OnClose:", c.RemoteAddr().String(), err)
 	})
@@ -41,39 +39,11 @@ func onWebsocket(c echo.Context) error {
 }
 
 func main() {
-	flag.Parse()
-
 	e := echo.New()
 
 	e.GET("/hello", onHello)
 	e.GET("/ws", onWebsocket)
 
-	if *useStd {
-		serveStd(e)
-	} else {
-		serveNbio(e)
-	}
-}
-
-func serveStd(e http.Handler) {
-	svr := http.Server{
-		Addr:    "localhost:8080",
-		Handler: e,
-	}
-	go svr.ListenAndServe()
-
-	log.Println("serving std")
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	<-interrupt
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-	svr.Shutdown(ctx)
-}
-
-func serveNbio(e http.Handler) {
 	svr := nbhttp.NewServer(nbhttp.Config{
 		Network: "tcp",
 		Addrs:   []string{"localhost:8080"},
@@ -84,7 +54,7 @@ func serveNbio(e http.Handler) {
 		log.Fatalf("nbio.Start failed: %v\n", err)
 	}
 
-	log.Println("serving nbio")
+	log.Println("serving [labstack/echo] on [nbio]")
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
