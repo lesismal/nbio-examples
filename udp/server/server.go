5806245753421d487a34f3b74edf9942d321020e
lesismal <lesismal@gmail.com>
2022-09-10 00:42:37 +0800

    update udp server log to show conn session

--- udp/server/server.go
+++ udp/server/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/lesismal/nbio"
@@ -13,15 +13,18 @@ func main() {
 		Network:            "udp",
 		Addrs:              []string{"127.0.0.1:8888"},
 		MaxWriteBufferSize: 6 * 1024 * 1024,
-		UDPReadTimeout: time.Second,
+		UDPReadTimeout:     time.Second,
 	})
 
+	g.OnOpen(func(c *nbio.Conn) {
+		log.Printf("onOpen: [%p, %v]", c, c.RemoteAddr().String())
+	})
 	g.OnData(func(c *nbio.Conn, data []byte) {
 		log.Printf("onData: [%p, %v], %v", c, c.RemoteAddr().String(), string(data))
 		c.Write(append([]byte{}, data...))
 	})
 	g.OnClose(func(c *nbio.Conn, err error) {
-		log.Printf("onClose: [%v], %v", c.RemoteAddr().String(), err)
+		log.Printf("onClose: [%p, %v], %v", c, c.RemoteAddr().String(), err)
 	})
 
 	err := g.Start()
