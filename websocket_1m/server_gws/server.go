29d3dfad88e015eddb60c916072b87030e3d8978
lesismal <lesismal@gmail.com>
2023-01-15 13:32:52 +0800

    Create server.go

---
+++ websocket_1m/server_gws/server.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"sync/atomic"
+	"time"
+
+	"github.com/lxzan/gws"
+)
+
+var (
+	qps   uint64 = 0
+	total uint64 = 0
+)
+
+func serve(addrs []string) {
+	for _, v := range addrs {
+		go func(addr string) {
+			var config = &gws.Config{
+				CompressEnabled:   true,
+				CheckTextEncoding: true,
+				MaxContentLength:  32 * 1024 * 1024,
+			}
+			var handler = new(WebSocket)
+			mux := &http.ServeMux{}
+			mux.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
+				socket, err := gws.Accept(writer, request, handler, config)
+				if err != nil {
+					return
+				}
+				socket.Listen()
+			})
+			server := http.Server{
+				Addr:    addr,
+				Handler: mux,
+			}
+			fmt.Println("server exit:", server.ListenAndServe())
+		}(v)
+	}
+}
+
+type WebSocket struct{}
+
+func (c *WebSocket) OnClose(socket *gws.Conn, code uint16, reason []byte) {
+	// fmt.Printf("onclose: code=%d, payload=%s\n", code, string(reason))
+}
+
+func (c *WebSocket) OnError(socket *gws.Conn, err error) {
+	// fmt.Printf("onerror: err=%s\n", err.Error())
+}
+
+func (c *WebSocket) OnOpen(socket *gws.Conn) {
+	// println("connected")
+}
+
+func (c *WebSocket) OnPing(socket *gws.Conn, payload []byte) {
+	// fmt.Printf("onping: payload=%s\n", string(payload))
+	// socket.WritePong(payload)
+}
+
+func (c *WebSocket) OnPong(socket *gws.Conn, payload []byte) {}
+
+func (c *WebSocket) OnMessage(socket *gws.Conn, message *gws.Message) {
+	socket.WriteMessage(message.Typ(), message.Bytes())
+	message.Close()
+	atomic.AddUint64(&qps, 1)
+}
+
+func main() {
+	serve(addrs)
+	ticker := time.NewTicker(time.Second)
+	for i := 1; true; i++ {
+		<-ticker.C
+		n := atomic.SwapUint64(&qps, 0)
+		total += n
+		fmt.Printf("running for %v seconds, NumGoroutine: %v, qps: %v, total: %v\n", i, runtime.NumGoroutine(), n, total)
+	}
+}
+
+var addrs = []string{
+	"localhost:28001",
+	"localhost:28002",
+	"localhost:28003",
+	"localhost:28004",
+	"localhost:28005",
+	"localhost:28006",
+	"localhost:28007",
+	"localhost:28008",
+	"localhost:28009",
+	"localhost:28010",
+
+	"localhost:28011",
+	"localhost:28012",
+	"localhost:28013",
+	"localhost:28014",
+	"localhost:28015",
+	"localhost:28016",
+	"localhost:28017",
+	"localhost:28018",
+	"localhost:28019",
+	"localhost:28020",
+
+	"localhost:28021",
+	"localhost:28022",
+	"localhost:28023",
+	"localhost:28024",
+	"localhost:28025",
+	"localhost:28026",
+	"localhost:28027",
+	"localhost:28028",
+	"localhost:28029",
+	"localhost:28030",
+
+	"localhost:28031",
+	"localhost:28032",
+	"localhost:28033",
+	"localhost:28034",
+	"localhost:28035",
+	"localhost:28036",
+	"localhost:28037",
+	"localhost:28038",
+	"localhost:28039",
+	"localhost:28040",
+
+	"localhost:28041",
+	"localhost:28042",
+	"localhost:28043",
+	"localhost:28044",
+	"localhost:28045",
+	"localhost:28046",
+	"localhost:28047",
+	"localhost:28048",
+	"localhost:28049",
+	"localhost:28050",
+}
