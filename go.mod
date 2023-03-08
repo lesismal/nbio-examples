e8aa5986965d9724ebd178c77729dbae9761cc9c
lesismal <lesismal@gmail.com>
2023-03-08 21:33:50 +0800

    + iris server

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/lesismal/nbio-examples
 go 1.16
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/antlabs/tinyws v0.0.7
 	github.com/buaazp/fasthttprouter v0.1.1
 	github.com/dgrr/fastws v1.0.4
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/kataras/iris/v12 v12.1.8
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/lesismal/llib v1.1.10
@@ -16,7 +20,15 @@ require (
 	github.com/lxzan/gws v1.3.1
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/sergi/go-diff v1.3.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
 	github.com/valyala/fasthttp v1.44.0
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
---
+++ http_with_other_frameworks/iris_server/iris_server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/kataras/iris/v12"
+	"github.com/lesismal/nbio/nbhttp"
+	"github.com/lesismal/nbio/nbhttp/websocket"
+)
+
+func onHello(ctx iris.Context) {
+	ctx.WriteString("hello world")
+}
+
+func onWebsocket(ctx iris.Context) {
+	upgrader := websocket.NewUpgrader()
+	upgrader.OnMessage(func(conn *websocket.Conn, messageType websocket.MessageType, data []byte) {
+		// echo
+		conn.WriteMessage(messageType, data)
+	})
+	upgrader.OnOpen(func(conn *websocket.Conn) {
+		log.Println("OnOpen:", conn.RemoteAddr().String())
+	})
+
+	conn, err := upgrader.Upgrade(ctx.ResponseWriter(), ctx.Request(), nil)
+	if err != nil {
+		panic(err)
+	}
+	wsConn := conn.(*websocket.Conn)
+	wsConn.OnClose(func(c *websocket.Conn, err error) {
+		log.Println("OnClose:", c.RemoteAddr().String(), err)
+	})
+}
+
+func main() {
+	app := iris.New()
+	app.Get("/hello", onHello)
+	app.Get("/ws", onWebsocket)
+
+	err := app.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	svr := nbhttp.NewServer(nbhttp.Config{
+		Network: "tcp",
+		Addrs:   []string{"localhost:8080"},
+		Handler: app,
+	})
+
+	err = svr.Start()
+	if err != nil {
+		log.Fatalf("nbio.Start failed: %v\n", err)
+	}
+
+	log.Println("serving [go-chi/chi] on [nbio]")
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	svr.Shutdown(ctx)
+}
